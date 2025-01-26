f1672431463f1742f1e0d23ecbac9c505d7a9c81
DoktorNauk44 <konradjura2003@gmail.com>
2025-01-26 21:51:24 +0100

    Update mainwindow.cpp

--- JPO_Fin/mainwindow.cpp
+++ JPO_Fin/mainwindow.cpp
@@ -1,6 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+//Kod GUI Na Podstawie Wygenerowanego przez ChatGPT oraz wykonanego przez "ProgrammingKnowledge" - https://www.youtube.com/@ProgrammingKnowledge
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
